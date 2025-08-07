b29472bf4f5936e5785a7b6a564616562db7e067
yeomfa <yeomfa@gmail.com>
2025-08-07 17:34:51 -0500

    Fix information lang and temporal dark theme

--- components/common/CategoryCard.tsx
+++ components/common/CategoryCard.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const CategoryCard = ({ title, children = <></> }: Props) => {
   return (
-    <article className="bg-white border border-primary p-4">
+    <article className="border border-primary p-4">
       <h3 className="font-semibold text-lg">{title}</h3>
       {children && (
         <div className="mt-2 flex flex-wrap gap-2 items-center">{children}</div>
