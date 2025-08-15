d350974d8ddd56857b169a74a8dda191d8de04a1
yeomfa <yeomfa@gmail.com>
2025-08-14 20:08:37 -0500

    Add ProjectCard component and integrate project data

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -1,120 +1,24 @@
-'use client';
-
+import ProjectCard from '@/components/common/ProjectCard';
 import SectionLayout from '@/components/Layouts/SectionLayout';
 import { Section } from '@/types/Sections';
-import { ArrowSquareOutIcon, GithubLogoIcon } from '@phosphor-icons/react';
-import Image from 'next/image';
-import Link from 'next/link';
 
-const project = {
-  image: 'https://yeomfa.github.io/vesto/public/vesto-favicon.png',
-  title: 'Vesto',
-  technologies: ['html', 'css', 'javascript'],
-  description: 'Vesto is an application to solve optimization problems',
-  respository: 'https://github.com/yeomfa/vesto',
-  liveSite: 'https://yeomfa.github.io/vesto',
-};
+import projects from '@/data/projects.json';
 
 const Projects = () => {
   return (
     <SectionLayout sectionId={Section.Projects} title="Projects">
       <div className="my-20 mb-100 grid gap-30 lg:grid-cols-2">
-        <article>
-          <div className="relative w-full h-50 aspect-video overflow-hidden mb-4">
-            <Image
-              src={project.image}
-              fill
-              alt={project.title}
-              className="object-cover"
-            ></Image>
-          </div>
-          <div className="flex flex-col gap-5">
-            <h3 className="font-bold text-2xl">{project.title}</h3>
-            <ul className="flex gap-3">
-              {project.technologies.map((tech) => (
-                <li key={tech}>
-                  <Image
-                    src={`/icons/${tech}.svg`}
-                    width={20}
-                    height={20}
-                    alt={tech}
-                  />
-                </li>
-              ))}
-            </ul>
-
-            <p>
-              {project.description}
-              {project.description}
-              {project.description}
-            </p>
-
-            <div className="flex gap-4 text-2xl">
-              <Link
-                href={project.respository}
-                className="text-primary"
-                target="blank"
-              >
-                <GithubLogoIcon weight="bold" />
-              </Link>
-              <Link
-                href={project.liveSite}
-                className="text-primary"
-                target="blank"
-              >
-                <ArrowSquareOutIcon weight="bold" />
-              </Link>
-            </div>
-          </div>
-        </article>
-        <article>
-          <div className="relative w-full h-50 aspect-video overflow-hidden mb-4">
-            <Image
-              src={project.image}
-              fill
-              alt={project.title}
-              className="object-cover"
-            ></Image>
-          </div>
-          <div className="flex flex-col gap-5">
-            <h3 className="font-bold text-2xl">{project.title}</h3>
-            <ul className="flex gap-3">
-              {project.technologies.map((tech) => (
-                <li key={tech}>
-                  <Image
-                    src={`/icons/${tech}.svg`}
-                    width={20}
-                    height={20}
-                    alt={tech}
-                  />
-                </li>
-              ))}
-            </ul>
-
-            <p>
-              {project.description}
-              {project.description}
-              {project.description}
-            </p>
-
-            <div className="flex gap-4 text-2xl">
-              <Link
-                href={project.respository}
-                className="text-primary"
-                target="blank"
-              >
-                <GithubLogoIcon weight="bold" />
-              </Link>
-              <Link
-                href={project.liveSite}
-                className="text-primary"
-                target="blank"
-              >
-                <ArrowSquareOutIcon weight="bold" />
-              </Link>
-            </div>
-          </div>
-        </article>
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            imagePath={project.imagePath}
+            description={project.description}
+            technologies={project.technologies}
+            liveSitePath={project.liveSitePath}
+            repositoryPath={project.repositoryPath}
+          />
+        ))}
       </div>
     </SectionLayout>
   );
